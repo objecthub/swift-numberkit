236f319abcf1ff02abc48851d27f7ffd0320146a
Matthias Zenger <matthias@objecthub.net>
2015-08-11 23:55:48 +0200

    Initial Commit

---
+++ NumberKit/NumberKit.h
@@ -0,0 +1,19 @@
+//
+//  NumberKit.h
+//  NumberKit
+//
+//  Created by Matthias Zenger on 11/08/2015.
+//  Copyright © 2015 ObjectHub. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NumberKit.
+FOUNDATION_EXPORT double NumberKitVersionNumber;
+
+//! Project version string for NumberKit.
+FOUNDATION_EXPORT const unsigned char NumberKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NumberKit/PublicHeader.h>
+
+
